895d681aa28c6dfa3864779a5ad2cf38797224a4
TOK KAI XIAN <kaixianp@gmail.com>
2021-01-12 01:23:19 +0800

    Update the Calendar model

--- lib/models/Calendar.dart
+++ lib/models/Calendar.dart
@@ -1,9 +1,10 @@
+import 'package:alpha_schedule/models/Event.dart';
 import 'package:flutter/cupertino.dart';
 
 class Calendar {
   String calendarName;
   String description;
   Color color;
-
+  List<Event> eventList;
   Calendar({this.calendarName, this.description, this.color});
 }
