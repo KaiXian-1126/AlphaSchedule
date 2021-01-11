86a68b7dcd5ca803bf9d27f5f0d3a7e9fac40593
TOK KAI XIAN <kaixianp@gmail.com>
2021-01-12 01:22:05 +0800

    Update the Event model

--- lib/models/Event.dart
+++ lib/models/Event.dart
@@ -1,5 +1,9 @@
+import 'package:flutter/material.dart';
+
 class Event {
   String eventName;
-
+  DateTime calendar;
+  TimeOfDay startTime, endTime;
+  String description;
   Event({this.eventName});
 }
