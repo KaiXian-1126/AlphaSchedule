46d3eab7bfa754f53a4923e04d045ce6919a7864
TOK KAI XIAN <kaixianp@gmail.com>
2021-01-16 09:15:24 +0800

    Update the provider used in the main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,7 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return Provider<ValueNotifier<User>>(
+    return ChangeNotifierProvider<ValueNotifier<User>>(
       create: (_) => ValueNotifier<User>(null),
       child: MaterialApp(
         onGenerateRoute: createRoute,
