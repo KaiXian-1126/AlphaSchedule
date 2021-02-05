0b72a558bc16a846b5bf8de8e7583f46b519be15
ChengShinWei <shinweiicheng@yahoo.com>
2021-02-05 17:14:32 +0800

    save change

--- lib/services/event/event_service_rest.dart
+++ lib/services/event/event_service_rest.dart
@@ -3,10 +3,33 @@ import 'package:alpha_schedule/models/Event.dart';
 import 'package:alpha_schedule/services/event/event_service.dart';
 import 'package:alpha_schedule/services/rest_service.dart';
 import 'package:alpha_schedule/app/dependencies.dart' as di;
+import 'package:intl/intl.dart';
 
 class EventServiceRest implements EventService {
   RestService rest = di.dependency();
 
+  /* Future<List<Event>> getEventList(
+      {Calendar c, DateTime date, DateTime currentTime}) async {
+    // convert dateTime to string
+    String selectedDate = DateFormat('yyyy-MM-dd').format(date);
+    //Get the event information in json form
+    final result = await rest.get("/event/getList/${c.calendarId}");
+    //Get the event information and push to list
+    List<Event> list = (result as List).map((e) => Event.fromJson(e)).toList();
+    print(list.length);
+
+    List<Event> test = [];
+    for (int i = 0; i < list.length; i++) {
+      if (list[i].calendar.compareTo(selectedDate) == 0) {
+        test.add(list[0]);
+      }
+    }
+    print(test.length);
+    List<Event> eventList = [];
+
+    //Return the event list
+    return test;
+  }*/
   Future<List<Event>> getEventList(
       {Calendar c, DateTime date, DateTime currentTime}) async {
     //Get the event information in json form
