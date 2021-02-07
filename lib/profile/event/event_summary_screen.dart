da2e431101270d1d03d4fcd1c808f05ad4465447
ChengShinWei <shinweiicheng@yahoo.com>
2021-02-08 02:53:18 +0800

    Fixed summary screen

--- lib/profile/event/event_summary_screen.dart
+++ lib/profile/event/event_summary_screen.dart
@@ -1,13 +1,13 @@
+import 'package:alpha_schedule/models/Calendar.dart';
+import 'package:alpha_schedule/services/event/event_service.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
+import 'package:provider/provider.dart';
 import '../../constants.dart';
+import 'package:alpha_schedule/app/dependencies.dart' as di;
 import '../../models/Event.dart';
 
 class EventSummaryScreen extends StatefulWidget {
-  final calender;
-
-  EventSummaryScreen(this.calender);
-
   @override
   _EventSummaryScreenState createState() => _EventSummaryScreenState();
 }
@@ -16,15 +16,16 @@ class _EventSummaryScreenState extends State<EventSummaryScreen> {
   String todayDate;
   List<Event> todayEvent;
   List<String> onComingEvent2;
+  EventService eventDependency = di.dependency();
 
   BoxDecoration myBoxDecoration() {
     return BoxDecoration(border: Border.all(), color: Colors.blue[100]);
   }
 
-  List<Event> getTodayEvent(String value) {
-    var dateParse = DateTime.parse(value);
+  List<Event> getTodayEvent(String date, eventList) {
+    var dateParse = DateTime.parse(date);
     List<Event> event = [];
-    widget.calender.eventList.forEach((item) {
+    eventList.forEach((item) {
       String a = '${item.calendar}';
       if (a.compareTo(dateParse.toString()) == 0) event.add(item);
     });
@@ -34,11 +35,11 @@ class _EventSummaryScreenState extends State<EventSummaryScreen> {
   }
 
   // To get list of task's date that greater than today's date without repeat same date
-  List<String> getOnComing2(value) {
-    var dateParse = DateTime.parse(value);
+  List<String> getOnComing2(date, eventList) {
+    var dateParse = DateTime.parse(date);
     List<String> event2 = [];
     List<String> temporary = [];
-    widget.calender.eventList.forEach((item) {
+    eventList.forEach((item) {
       String a = '${item.calendar}';
       if (a.compareTo(dateParse.toString()) > 0) {
         temporary.add(a);
@@ -53,68 +54,80 @@ class _EventSummaryScreenState extends State<EventSummaryScreen> {
   @override
   void initState() {
     super.initState();
-    todayDate = getDate(DateTime.now());
-    onComingEvent2 = getOnComing2(todayDate);
+    todayDate = DateFormat('yyyy-MM-dd').format(DateTime.now());
   }
 
   @override
   Widget build(BuildContext context) {
+    final Calendar c =
+        Provider.of<ValueNotifier<Calendar>>(context, listen: false).value;
     return Scaffold(
       appBar: AppBar(
         leading: IconButton(
           icon: Icon(Icons.arrow_back),
           color: Colors.white,
-          onPressed: () => Navigator.pop(context, widget.calender),
+          onPressed: () => Navigator.pop(context),
         ),
         title: Text(
-          widget.calender.calendarName,
+          c.calendarName,
           style: TextStyle(color: Colors.white),
         ),
         backgroundColor: Colors.blue,
       ),
-      body: ListView(
-        children: [
-          Container(
-            decoration: myBoxDecoration(),
-            child: ListTile(
-              tileColor: Colors.blue[300],
-              title: Container(
-                padding: EdgeInsets.only(left: 20),
-                child: Text(
-                  'Today',
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontWeight: FontWeight.bold,
-                    fontSize: 20,
+      body: FutureBuilder(
+          future: eventDependency.getEventList(c: c),
+          builder: (context, snapshot) {
+            if (snapshot.hasData) {
+              onComingEvent2 = getOnComing2(todayDate, snapshot.data);
+              return ListView(
+                children: [
+                  Container(
+                    decoration: myBoxDecoration(),
+                    child: ListTile(
+                      tileColor: Colors.blue[300],
+                      title: Container(
+                        padding: EdgeInsets.only(left: 20),
+                        child: Text(
+                          'Today',
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontWeight: FontWeight.bold,
+                            fontSize: 20,
+                          ),
+                        ),
+                      ),
+                    ),
                   ),
-                ),
-              ),
-            ),
-          ),
-          myList(getTodayEvent(todayDate), Colors.blue[100]),
-          Container(
-            decoration: myBoxDecoration(),
-            child: ListTile(
-              tileColor: Colors.blue[300],
-              title: Container(
-                padding: EdgeInsets.only(left: 20),
-                child: Text(
-                  'On Coming',
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontWeight: FontWeight.bold,
-                    fontSize: 20,
+                  myList(getTodayEvent(todayDate, snapshot.data),
+                      Colors.blue[100]),
+                  Container(
+                    decoration: myBoxDecoration(),
+                    child: ListTile(
+                      tileColor: Colors.blue[300],
+                      title: Container(
+                        padding: EdgeInsets.only(left: 20),
+                        child: Text(
+                          'On Coming',
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontWeight: FontWeight.bold,
+                            fontSize: 20,
+                          ),
+                        ),
+                      ),
+                    ),
                   ),
-                ),
-              ),
-            ),
-          ),
 
-          /// oncoming listview
-          for (int i = 0; i < onComingEvent2.length; i++)
-            myList(getTodayEvent(onComingEvent2[i]), Colors.blue[100]),
-        ],
-      ),
+                  /// oncoming listview
+                  for (int i = 0; i < onComingEvent2.length; i++)
+                    myList(getTodayEvent(onComingEvent2[i], snapshot.data),
+                        Colors.blue[100]),
+                ],
+              );
+            } else {
+              return Center(child: CircularProgressIndicator());
+            }
+          }),
     );
   }
 
@@ -124,83 +137,73 @@ class _EventSummaryScreenState extends State<EventSummaryScreen> {
       return BoxDecoration(border: Border.all(), color: Colors.grey[300]);
     }
 
-    return Container(
-      margin: EdgeInsets.only(left: 20, bottom: 20, right: 20),
-      child: ListView.builder(
-        shrinkWrap: true,
-        physics: ScrollPhysics(),
-        itemCount: a.length,
-        itemBuilder: (context, index) {
-          return Column(
-            children: [
-              Center(
-                child: Container(child: myText(a[index].calendar, index)),
-              ),
-              Container(
-                  decoration: myBoxDecoration(),
-                  child: Card(
-                    child: ListTile(
-                        leading: Container(
-                          decoration: BoxDecoration(color: color),
-                          child: Container(
-                            width: 120,
-                            child: Text(
-                              // "${widget.event.timeToStringConverter(widget.event.startTime)} to ${widget.event.timeToStringConverter(widget.event.endTime)}",
-                              '${a[index].timeToStringConverter(a[index].startTime)}' +
-                                  "\nto\n" +
-                                  '${a[index].timeToStringConverter(a[index].endTime)}',
-                              textAlign: TextAlign.center,
+    if (a.length == 0) {
+      return null;
+    } else {
+      return Container(
+        margin: EdgeInsets.only(left: 20, bottom: 20, right: 20),
+        child: ListView.builder(
+          shrinkWrap: true,
+          physics: ScrollPhysics(),
+          itemCount: a.length,
+          itemBuilder: (context, index) {
+            return Column(
+              children: [
+                Center(
+                  child: Container(child: myText(a[index].calendar, index)),
+                ),
+                Container(
+                    decoration: myBoxDecoration(),
+                    child: Card(
+                      child: ListTile(
+                          leading: Container(
+                            decoration: BoxDecoration(color: color),
+                            child: Container(
+                              width: 120,
+                              child: Text(
+                                // "${widget.event.timeToStringConverter(widget.event.startTime)} to ${widget.event.timeToStringConverter(widget.event.endTime)}",
+                                '${a[index].timeToStringConverter(a[index].startTime)}' +
+                                    "\nto\n" +
+                                    '${a[index].timeToStringConverter(a[index].endTime)}',
+                                textAlign: TextAlign.center,
+                              ),
                             ),
                           ),
-                        ),
-                        title: Text(
-                          a[index].eventName,
-                          style: TextStyle(fontSize: 20),
-                        ),
-                        onTap: () async {
-                          final respond = await Navigator.pushNamed(
-                            context,
-                            eventDetailsRoute,
-                            arguments: a[index],
-                          );
-                          if (respond != null) {
-                            setState(() {});
-                          }
-                        }),
-                  )),
-            ],
-          );
-        },
-      ),
-    );
+                          title: Text(
+                            a[index].eventName,
+                            style: TextStyle(fontSize: 20),
+                          ),
+                          onTap: () async {
+                            final respond = await Navigator.pushNamed(
+                              context,
+                              eventDetailsRoute,
+                              arguments: a[index],
+                            );
+                            if (respond != null) {
+                              setState(() {});
+                            }
+                          }),
+                    )),
+              ],
+            );
+          },
+        ),
+      );
+    }
   }
 }
 
 Widget myText(DateTime date, index) {
-  var date1 = getDate(date);
+  var date1 = DateFormat('yyyy-MM-dd').format(date);
   var day = DateFormat('EEEE').format(date);
   if (index == 0) {
     return Padding(
-      padding: const EdgeInsets.only(right: 8, left: 20, top: 12, bottom: 12),
+      padding: const EdgeInsets.only(right: 8, left: 0, top: 12, bottom: 12),
       child: Text(
-        "$date1   $day",
+        "$date1     $day",
         style: TextStyle(fontSize: 20),
       ),
     );
   }
   return null;
-}
-
-String getDate(DateTime value) {
-  var dateParse = DateTime.parse(value.toString());
-  String month = dateParse.month.toString();
-  String day = dateParse.day.toString();
-  if (month.length != 2) {
-    month = "0$month";
-  }
-  if (day.length != 2) {
-    day = "0$day";
-  }
-  String a = ("${dateParse.year}-$month-$day");
-  return a;
 }
--- lib/profile/home_screen.dart
+++ lib/profile/home_screen.dart
@@ -332,11 +332,7 @@ class _DrawerScreenState extends State<DrawerScreen> {
                 onTap: (index) async {
                   _currentIndex = index;
                   if (index == 1) {
-                    final response =
-                        await Navigator.pushNamed(context, eventSummaryRoute);
-                    if (response != null) {
-                      setState(() {});
-                    }
+                    await Navigator.pushNamed(context, eventSummaryRoute);
                   } else if (index == 2) {
                     final response = await Navigator.pushNamed(
                         context, calendarCollaboratorRoute,
